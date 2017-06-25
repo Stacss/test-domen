8f5d026d1d6caae471c57d15f66360df239a00e9
Stacss <postnikov.sa@ya.ru>
2017-06-25 20:45:13 +0300

    тест гита

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>работа с PHP</title>
+</head>
+<body>
+	<?php 
+	echo '<h1>hello world! this is php!</h1>'
+	?>
+	<p>test git</p>
+</body>
+</html>
