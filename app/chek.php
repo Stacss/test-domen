5c5734274efbb02746e7319dcc970461c30c44d9
Stacss <postnikov.sa@ya.ru>
2017-07-02 13:17:06 +0300

    добавил swith к маркам авто

--- app/chek.php
+++ app/chek.php
@@ -11,15 +11,52 @@
 			
 			echo "марка: ".$name."<br>"."модель: ".$model."<br>"."год: ".$year."<br>"."VIN: ".$vin."<br>";
 			if ($year<=2000) {
-			echo 'Ваша машина довольно стара, пора ее на свалку';
-			};
-			if ($yaer<=2010 and $year>=2001) {
-			echo 'Ваша машина еще более-менее, но будет часто ломаться';
-			};
-			if ($year>=2011) {
-			echo 'Ваша машина еще хорошая';
+				echo 'Ваша машина довольно стара, пора ее на свалку!<br>';
 			}
-		
+			elseif ($year>=2011){
+				echo 'Ваша машина еще хорошая!<br>';
+			}
+			else {
+				echo "машина неплохая, но будет ломаться!<br>";
+			};
+			switch ($name) {
+				case "bmw":
+					echo $name." - это европейская авто!<br>";
+					break;
+				case "audi":
+					echo $name." - это европейская авто!<br>";
+					break;
+				case "skoda":
+					echo $name." - это европейская авто!<br>";
+					break;
+				case "volkswagen":
+					echo $name." - это европейская авто!<br>";
+					break;
+				case "porsche":
+					echo $name." - это европейская авто!<br>";
+					break;
+				case "nissan":
+					echo $name." - это японская авто!<br>";
+					break;
+				case "toyota":
+					echo $name." - это японская авто!<br>";
+					break;
+				case "mazda":
+					echo $name." - это японская авто!<br>";
+					break;
+				case "mitsubishi":
+					echo $name." - это японская авто!<br>";
+					break;
+				case "kia":
+					echo $name." - это корейская авто!<br>";
+					break;
+				case "hyundai":
+					echo $name." - это корейская авто!<br>";
+					break;
+				case "daewoo":
+					echo $name." - это корейская авто!<br>";
+					break;
+			}
 	?>
 	<?php
 		
